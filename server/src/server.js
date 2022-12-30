45e1d81baab4c0fb21dd12076219aca6a4602511
HiepThanh <hiepthanha7@gmail.com>
2022-12-30 15:55:05 +0700

    try connect with DB(mySQL)

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import bodyParser from "body-parser"
 import viewEngine from "./config/viewEngine"
 import initWebRouters from "./route/web"
+import connectDB from './config/connectDB'
+
 require('dotenv').config();
 
 let app = express()
@@ -12,6 +14,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 viewEngine(app)
 initWebRouters(app)
+
+connectDB();
 
 let port = process.env.PORT ||6969;
 
